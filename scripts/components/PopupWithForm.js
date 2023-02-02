5d62d974f422b93eaba01688b7557599e4fb86c7
Work desktop <the.red.lemon@yandex.ru>
2023-02-02 15:11:28 +0300

    feat: настроить логику popup addCard через class PopupWithForm

--- scripts/components/PopupWithForm.js
+++ scripts/components/PopupWithForm.js
@@ -2,8 +2,33 @@ import Popup from "./Popup.js";
 
 export default
 class PopupWithForm extends Popup {
-  constructor(popupSelector, submitForm) {
+  constructor(popupSelector, handleFormSubmit) {
     super(popupSelector);
-    this._submitForm = submitForm;
+    this._handleFormSubmit = handleFormSubmit;
+    this._form = this._popup.querySelector('.popup__form');
+  }
+
+  _getInputValues = () => {
+    this._inputFormValues = {
+      name: this._form.querySelector('.popup__input_type_name').value,
+      link: this._form.querySelector('.popup__input_type_about').value
+    };
+
+    console.log(this._inputFormValues)
+    return this._inputFormValues;
+  }
+
+  setEventListeners = () => {
+    super.setEventListeners();
+
+    this._form.addEventListener('submit', () => {
+      this._handleFormSubmit(this._getInputValues());
+    });
+  }
+
+  close() {
+    super.close();
+
+    this._form.reset()
   }
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -5,15 +5,38 @@ import UserInfo from '../components/UserInfo.js';
 import Section from '../components/Section.js';
 import FormValidator from '../components/FormValidator.js';
 import Card from '../components/Card.js';
+import PopupWithForm from '../components/PopupWithForm.js';
 import PopupWithImage from '../components/PopupWithImage.js';
-import Popup from '../components/Popup.js';
 
 const profileFormValidator = new FormValidator(all.formSetting, all.formEditProfile),
       cardFormValidator = new FormValidator(all.formSetting, all.formAddCard),
       userInfo = new UserInfo(all.userInfoData),
-      profilePopup = new Popup(all.popupSelectors.profilePopup),
-      cardsPopup = new Popup(all.popupSelectors.cardsPopup),
       popupWithImage = new PopupWithImage(all.popupSelectors.imgPopup);
+
+
+
+const profilePopup = new PopupWithForm(
+      all.popupSelectors.profilePopup);
+
+const cardsPopup = new PopupWithForm(
+      all.popupSelectors.cardsPopup,
+      (inputFormValues) => {
+        cardFormValidator.disableSubmitButton();
+
+        const userCard = new Section({
+          data: [inputFormValues],
+          renderer: (item) => {
+            const card = new Card(item, all.cardSetting);
+            const cardElement = card.generateCard();
+            userCard.addItem(cardElement)
+          }
+        }, all.cardsContainer);
+
+        userCard.renderCards();
+
+        cardsPopup.close();
+      }
+      );
 
 const defaultCardList = new Section({
   data: all.initialCards,
@@ -27,33 +50,6 @@ const defaultCardList = new Section({
     defaultCardList.addItem(cardElement)
   }
 }, all.cardsContainer);
-
-
-
-
-
-function handleCardFormSubmit(evt) {
-  const cardElement = {
-    name: all.inputCardName.value,
-    link: all.inputCardLink.value
-  }
-
-  evt.target.reset();
-  cardFormValidator.disableSubmitButton();
-
-  const userCard = new Section({
-    data: [cardElement],
-    renderer: (item) => {
-      const card = new Card(item, all.cardSetting);
-      const cardElement = card.generateCard();
-      userCard.addItem(cardElement)
-    }
-  }, all.cardsContainer);
-
-  userCard.renderCards();
-
-  cardsPopup.close();
-}
 
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
@@ -74,8 +70,6 @@ function openProfileEditPopup(evt) {
   profilePopup.open();
 }
 
-
-
 all.profileEditBtn.addEventListener('click', openProfileEditPopup);
 
 all.cardsAddBtn.addEventListener('click', () => {
@@ -83,7 +77,6 @@ all.cardsAddBtn.addEventListener('click', () => {
 });
 
 all.formEditProfile.addEventListener('submit', handleProfileFormSubmit);
-all.formAddCard.addEventListener('submit', handleCardFormSubmit);
 
 profileFormValidator.enableValidation();
 cardFormValidator.enableValidation();
