0ad77e3c12d743de9431a21646b6c40c15df32b8
laptop <the.red.lemon@yandex.ru>
2023-02-03 21:31:37 +0300

    fix: исправить classes Popup и PopupWithImage после ревью

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -4,8 +4,7 @@ export default
 class Popup {
   constructor(popupSelector) {
     this._popup = document.querySelector(popupSelector);
-    this._popupBigImage = this._popup.querySelector('.popup__img');
-    this._popupTitle = this._popup.querySelector('.popup__title');
+    this._handleEscClose = this._handleEscClose.bind(this);
   }
 
   _handleEscClose(event) {
@@ -16,9 +15,7 @@ class Popup {
 
   setEventListeners() {
     this._popup.addEventListener('mousedown', (evt) => {
-      if (evt.target.classList.contains('popup_opened')) {
-        this.close();
-      } else if (evt.target.classList.contains('popup__close')) {
+      if (evt.target.classList.contains('popup_opened') || evt.target.classList.contains('popup__close')) {
         this.close();
       }
     })
@@ -27,12 +24,12 @@ class Popup {
   open() {
     this._popup.classList.add('popup_opened');
 
-    document.addEventListener('keydown', this._handleEscClose.bind(this))
+    document.addEventListener('keydown', this._handleEscClose)
   }
 
   close() {
     this._popup.classList.remove('popup_opened');
 
-    document.removeEventListener('keydown', this._handleEscClose.bind(this));
+    document.removeEventListener('keydown', this._handleEscClose);
   }
 }
--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -4,6 +4,13 @@ import Popup from './Popup.js'
 
 export default
 class PopupWithImage extends Popup {
+  constructor(popupSelector) {
+    super(popupSelector);
+
+    this._popupBigImage = this._popup.querySelector('.popup__img');
+    this._popupTitle = this._popup.querySelector('.popup__title');
+  }
+
   open(img, title) {
     super.open();
 
